1ec031da6112ac02d32c52f843571d77cd952772
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-11-15 11:55:23 +0100

    initiation

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,13 @@
+/**
+ * binary_tree_node - a fonction
+ *
+ * @parent: Pointer to noeud
+ * @value: the int
+ *
+ * Return: avalu
+ */
+ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+ {
+	return NULL;
+ }
+ 
